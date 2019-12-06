20c06e83e8d1e3aa3de2cc5d13e86f4e28685351
Kevin Nguyen <kn1120@nyu.edu>
2019-12-05 19:00:36 -0500

    adding gauresh Filter components

--- plutus-frontend/src/App.js
+++ plutus-frontend/src/App.js
@@ -1,45 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Dropdown from './components/Dropdown.js';
+import Chart from './components/Chart.js';
+
 import './App.css';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = { 
+      apiResponse: "",
+      currCompany: null,
+      currLevel: null,
+      companies: [
+        {
+          id:1,
+          value: "Microsoft"
+        },
+        {
+          id:2,
+          value: "Google"
+        },
+        {
+          id:3,
+          value: "Facebook"
+        },
+        {
+          id:4,
+          value: "Lyft"
+        },
+        {
+          id:5,
+          value: "Amazon"
+        },
+      ],
+      levels: [
+        {
+          id:1,
+          value: "Entry"
+        },
+        {
+          id:2,
+          value: "Senior"
+        },
+        {
+          id:3,
+          value: "New Grad"
+        },
+        {
+          id:4,
+          value: "Partner"
+        },
+        
+      ],      
+     };
   }
 
-  callAPI() {
-      fetch("http://localhost:9000/")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
+  companyCallback = (companyData) => {
+
+    this.setState({currCompany: companyData});
+
   }
 
-  componentWillMount() {
-      this.callAPI();
+  levelCallback = (levelData) => {
+
+    this.setState({currLevel: levelData});
+
   }
 
 
-  render () {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi Bros, Have fun with React!.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"> 
+        <h1> App </h1>
+        <Dropdown type="Company" callback={this.companyCallback} opts={this.state.companies} />
+        <Dropdown type="Level" callback={this.levelCallback} opts={this.state.levels} />
+        <Chart company={this.state.currCompany} level={this.state.currLevel}/>
       </div>
-    );
-  }
 
+
+      )
+  }
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ plutus-frontend/src/components/Chart.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+
+import {Line, Bar} from 'react-chartjs-2';
+
+
+class Chart extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dummydata: {
+				default: {
+					labels: ['Mar 2019', 'Apr 2019', 'May 2019', 'Jun 2019', 'Jul 2019'],
+					datasets: [
+						{
+							label: "Salary",
+							data: [
+								60000,
+								60000,
+								60000,
+								60000,
+								60000
+							]
+						}					]
+				},
+				Microsoft: {
+					labels: ['Mar 2019', 'Apr 2019', 'May 2019', 'Jun 2019', 'Jul 2019'],
+					datasets: [
+						{
+							label: "Salary",
+							data: [
+								100000,
+								90000,
+								80000,
+								95000,
+								110000
+							]
+						}					]
+				},
+				Google: {
+					labels: ['Mar 2019', 'Apr 2019', 'May 2019', 'Jun 2019', 'Jul 2019'],
+					datasets: [
+						{
+							label: "Salary",
+							data: [
+								75000,
+								90000,
+								120000,
+								85000,
+								100000
+
+							]
+						}
+					]
+				}
+			}
+		}
+		
+	}
+
+	render() {
+		let working_set = this.state.dummydata.default;
+
+		if (this.props.company == "Microsoft") {
+			working_set = this.state.dummydata.Microsoft
+		}
+		if (this.props.company == "Google") {
+			working_set = this.state.dummydata.Google
+		}
+		return (
+
+			<div className="chart">
+				\/ CHART COMPONENT \/
+				<p> Company inside the chart: {this.props.company} </p>
+				<p> Level inside the chart: {this.props.level} </p>
+				
+
+				<p> Test {this.state.temp} </p>
+				<Line data={working_set} />
+				/\ CHART COMPONENT /\
+
+			</div>
+
+
+		);
+
+	}
+
+
+}
+
+export default Chart;
