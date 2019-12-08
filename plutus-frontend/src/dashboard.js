3181a19f18a87d092a8995bed7d4f84cb3295fb3
Deanna <dt1543@nyu.edu>
2019-12-07 20:49:01 -0500

    set up tentative homepage and refactor dashboard; link appropiately

--- plutus-frontend/src/dashboard.js
+++ plutus-frontend/src/dashboard.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
-import logo from './resources/logo.svg';
 import './css/Dashboard.css';
+import Dropdown from './components/Dropdown.js';
+import Chart from './components/Chart.js';
 
 class Dashboard extends Component {
+
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = {
+      apiResponse: "",
+      currCompany: null,
+      currLevel: null,
+      companies: [
+        {
+          id:1,
+          value: "Microsoft"
+        },
+        {
+          id:2,
+          value: "Google"
+        },
+        {
+          id:3,
+          value: "Facebook"
+        },
+        {
+          id:4,
+          value: "Lyft"
+        },
+        {
+          id:5,
+          value: "Amazon"
+        },
+      ],
+      levels: [
+        {
+          id:1,
+          value: "Entry"
+        },
+        {
+          id:2,
+          value: "Senior"
+        },
+        {
+          id:3,
+          value: "New Grad"
+        },
+        {
+          id:4,
+          value: "Partner"
+        },
+
+      ],
+     };
   }
 
   callAPIDashboard() {
@@ -19,15 +66,27 @@ class Dashboard extends Component {
   }
 
 
-  render () {
+  companyCallback = (companyData) => {
+
+    this.setState({currCompany: companyData});
+
+  }
+
+  levelCallback = (levelData) => {
+
+    this.setState({currLevel: levelData});
+  }
+
+
+  render() {
     return (
       <div className="Dashboard">
-        <header className="header">
-          <img src={logo} className="logo" alt="logo" />
-          <p className="intro">If this shows, it means {this.state.apiResponse}</p>
-        </header>
+        <h1> App </h1>
+        <Dropdown type="Company" callback={this.companyCallback} opts={this.state.companies} />
+        <Dropdown type="Level" callback={this.levelCallback} opts={this.state.levels} />
+        <Chart company={this.state.currCompany} level={this.state.currLevel}/>
       </div>
-    );
+    )
   }
 
 }
