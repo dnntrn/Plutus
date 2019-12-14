ac75fc0d1cc939089c95fed149f569583c693033
Kevin Nguyen <kn1120@nyu.edu>
2019-12-13 21:19:12 -0500

    adding new FullChart Component and Multiple,indepedent charts

--- plutus-frontend/src/dashboard.js
+++ plutus-frontend/src/dashboard.js
@@ -3,7 +3,8 @@ import './css/Dashboard.css';
 import Dropdown from './components/Dropdown.js';
 import NavBar from './components/NavBar.js';
 import Chart from './components/Chart.js';
-
+import {Row,Col} from 'react-bootstrap';
+import FullChart from './components/FullChart'
 class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -13,8 +14,7 @@ class Dashboard extends Component {
     currJobLevel:null,
     currKeywords:null,
     currMinSalary:null,
-    NumCharts:null,
-    chartArray:[],
+    NumCharts:1,
     companies: [
       {
         id:0,
@@ -109,27 +109,44 @@ class Dashboard extends Component {
     this.setState({currLevel: levelData});
   }
   NumChartsCallback = (num) => {
-
-    this.setState({NumCharts: num});
-  }
-  componentDidUpdate(){
-  
+    this.setState({
+      NumCharts: num,
+  });
 
   }
+
   render () {
+    let newArray =Array.from({ length: this.state.NumCharts }, (v, i) => i)
     return (
       <div className="Dashboard">
         <NavBar />
         <h1 style={{marginTop: "3%"}}> Dashboard </h1>
         <div id="allContentDash">
-          <div id="filterBox">
-            <Dropdown type ="NumCharts" callback ={this.NumChartsCallback} opts= {this.state.chartOptions}/>
-            <Dropdown type="Company" callback={this.companyCallback} opts={this.state.companies} />
-            <Dropdown type="Level" callback={this.levelCallback} opts={this.state.levels} />
-          </div>
-          <div id="chartBox">
-            <Chart company={this.state.currCompany} level={this.state.currLevel} />
-          </div>
+            <div>
+              <Dropdown type ="NumCharts" callback ={this.NumChartsCallback} opts= {this.state.chartOptions}/>
+            </div>
+          
+            {newArray.map(num =>
+            <>
+              <FullChart ddopts = {this.state.companies}/>
+            {/*
+            <Row>
+              <Col>
+                <div id="filterBox">
+                <Dropdown type="Company" callback={this.companyCallback} opts={this.state.companies} />
+                <Dropdown type="Level" callback={this.levelCallback} opts={this.state.levels} />
+                </div>
+              </Col>
+              <Col>
+            <div id="chartBox">
+              <Chart company={this.state.currCompany} level={this.state.currLevel} />
+            </div>
+            </Col>
+            </Row>*/}
+            
+
+            </>)} 
+  
         </div>
       </div>
       )
--- plutus-frontend/src/recommendations.js
+++ plutus-frontend/src/recommendations.js
@@ -6,6 +6,26 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import SearchResultBox from './components/SearchResultBox'
 
+
+const arrayData =[
+  {
+    id: '1 - Jerry',
+    name: 'Jerry'    
+  },
+  {
+    id: '2 - Elaine',
+    name: 'Elaine'    
+  },
+  {
+    id: '3 - Kramer',
+    name: 'Kramer'    
+  },
+  {
+    id: '4 - George',
+    name: 'George'    
+  },
+];
+
 class Recommendations extends Component {
   constructor(props) {
     super(props);
